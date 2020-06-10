9329916ea03b1c1c41f23130bef7c6ad194846b3
SAFNAS <iamsafnas@gmail.com>
2020-06-10 15:25:12 +0530

    added model and migrated

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.app.PollConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

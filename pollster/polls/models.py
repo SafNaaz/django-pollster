8cf71d7ce117b4a4f5bddda0e14fe76be13cbff1
SAFNAS <iamsafnas@gmail.com>
2020-06-10 15:50:38 +0530

    proper get to show name in admin

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -7,9 +7,8 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
-
-def __str__(self):
-    return self.question_text
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -17,6 +16,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-
-def __str__(self):
-    return self.choice_text
+    def __str__(self):
+        return self.choice_text

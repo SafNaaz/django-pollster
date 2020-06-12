5a1297c8d6da4d9b01485c88e43abed9c8c75661
SAFNAS <iamsafnas@gmail.com>
2020-06-12 16:07:58 +0530

    show question in index

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -6,4 +6,6 @@ from .models import Question, Choice
 
 # Get questions and display them
 def index(request):
-    return render(request,'polls/index.html')
+    lates_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':lates_question_list}
+    return render(request,'polls/index.html',context)

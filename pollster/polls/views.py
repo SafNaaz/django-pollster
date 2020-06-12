26363294e934f177a8e9fb1c91cd3656a0d4385c
SAFNAS <iamsafnas@gmail.com>
2020-06-12 16:17:20 +0530

    detail route

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -9,3 +9,11 @@ def index(request):
     lates_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list':lates_question_list}
     return render(request,'polls/index.html',context)
+
+# Show specific question and choices
+def detail(request, question_id):
+    try:
+        question: Question.objects.get(pk=question_id)
+    except: Question.DoesNotExists
+        raise Http404("Question does not exist")
+    return render(request, 'polls/result.html',{'question':question})
